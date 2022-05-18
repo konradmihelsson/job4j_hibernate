be4e33ec0d1656d41341ffb847f2895121b51b8a
konradmihelsson <konrad.mihelsson@gmail.com>
2022-05-18 14:47:28 +0300

    Hibernate. Mapping. ToMany.

---
+++ src/main/java/ru/job4j/hibernate/HbnRunner.java
@@ -0,0 +1,45 @@
+package ru.job4j.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.hibernate.model.Make;
+import ru.job4j.hibernate.model.Model;
+
+public class HbnRunner {
+
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Model model1 = Model.of("Prius");
+            session.save(model1);
+            Model model2 = Model.of("Sequoia");
+            session.save(model2);
+            Model model3 = Model.of("Mark II");
+            session.save(model3);
+            Model model4 = Model.of("Avensis");
+            session.save(model4);
+            Model model5 = Model.of("Rav4");
+            session.save(model5);
+
+            Make make = Make.of("Toyota");
+            make.addModel(session.load(Model.class, 1));
+
+            session.save(make);
+
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
